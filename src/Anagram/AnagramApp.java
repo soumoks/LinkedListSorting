00d3f2a118175f5633160a263a2d518fdcded0f7
vaibhav50596 <vahbiav469882>
2019-11-28 15:42:56 -0700

    Implemented total time required to process input file

--- src/Anagram/AnagramApp.java
+++ src/Anagram/AnagramApp.java
@@ -8,6 +8,7 @@ public class AnagramApp {
 
     private AnagramLinkedList wordLinkedList;
     private ModelManager theManager;
+    long startTime, endTime = 0;
 
     public AnagramApp(){
         theManager = new ModelManager();
@@ -15,18 +16,29 @@ public class AnagramApp {
 
     public static void main(String [] args) {
         FileManager fileManager = new FileManager();
-        AnagramLinkedList words = fileManager.readFile();
         AnagramApp app = new AnagramApp();
+        app.startTime = System.nanoTime();
+        AnagramLinkedList words = fileManager.readFile();
+
         Word cursor = words.getHead();
+        System.out.println("Words from text file: \n");
         while(cursor != null){
+            System.out.println(cursor.getWordName());
             app.wordLinkedList = new AnagramLinkedList(new Word(cursor.getWordName()));
             app.theManager.addLinkedList(app.wordLinkedList);
             cursor = cursor.getNext();
         }
-        app.theManager.printWordList();
+        //app.theManager.printWordList();
         app.theManager.sort(app.theManager.getWordList(),0,app.theManager.getwordListSize() -1);
+        app.endTime = System.nanoTime();
         System.out.println();
-        System.out.println("Sorted List: ");
+        System.out.println("Sorted list of anagrams: ");
         app.theManager.printWordList();
+        long duration = app.calculateDuration(app.startTime, app.endTime);
+        System.out.println("\nTime required to process input file: " + duration);
+    }
+
+    public long calculateDuration(long startTime, long endTime){
+        return endTime - startTime;
     }
 }
